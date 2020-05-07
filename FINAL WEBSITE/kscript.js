8f563d6c0aac2add0eb09ee89b69d8c2bf2787c1
staicyngongu <60276222+staicyngongu@users.noreply.github.com>
2020-05-08 01:46:51 +0300

    S

---
+++ FINAL WEBSITE/kscript.js
@@ -0,0 +1,26 @@
+let interval
+
+const element = document.querySelector('.scramble')
+const originalText = element.innerText
+
+const randomInt = max => Math.floor(Math.random() * max)
+const randomFromArray = array => array[randomInt(array.length)]
+
+const scrambleText = text => {
+  const chars = '*?><[]&@#)(.%$-_:/;?!'.split('')
+  return text
+    .split('')
+    .map(x => randomInt(3) > 1 ? randomFromArray(chars) : x)
+    .join('')
+}
+  
+element.addEventListener('mouseover', () => {
+  interval = setInterval(() =>
+    element.innerText = scrambleText(originalText)
+  , 100)
+})
+  
+element.addEventListener('mouseout', () => {
+  clearInterval(interval)
+  element.innerText = originalText
+})
---
+++ FINAL WEBSITE/landingscript.js
@@ -0,0 +1,3 @@
+document.querySelector('.toggleClass').onclick = function(){
+            document.querySelector('.theTitle').classList.toggle('active');
+        };
